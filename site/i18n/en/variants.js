28f570ffc44b3a56a612ecef231f908544034589
Skrap87 <57953089+Skrap87@users.noreply.github.com>
2025-12-26 02:27:13 +0100

    Add files via upload

---
+++ site/i18n/en/variants.js
@@ -0,0 +1,10 @@
+window.I18N_PARTS = window.I18N_PARTS || {};
+window.I18N_PARTS.en = window.I18N_PARTS.en || {};
+window.I18N_PARTS.en.variants = {
+  variants: {
+    s: {},
+    m: {},
+    l: {},
+    xl: {}
+  }
+};
